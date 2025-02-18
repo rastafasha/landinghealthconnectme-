ff9e3b209082b1065066f90c67b11b69c2cecaba
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-02-17 20:27:38 -0400

    css

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   //local
-  apiUrl: "http://127.0.0.1:8000/api",
-  apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
+  // apiUrl: "http://127.0.0.1:8000/api",
+  // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
   //remoto
-  // apiUrl: "https://healt-connect.me/backend-api-landing/public/api",
-  // apiUrlMedia: "https://healt-connect.me/backend-api-landing/storage/app/",
+  apiUrl: "https://healt-connect.me/backend-api-landing/public/api",
+  apiUrlMedia: "https://healt-connect.me/backend-api-landing/storage/app/",
   // clientId: ''
 
 };
