3a5bd9b23a7bcf534175faad1222da329a1b2920
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-03-22 02:33:50 -0400

    camio nombre a klyntic

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
   // apiUrl: "http://127.0.0.1:8000/api",
   // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
   //remoto
-  apiUrl: "https://admin.health-connect.me/backend-api-landing/public/api",
-  apiUrlMedia: "https://admin.health-connect.me/backend-api-landing/storage/app/",
+  apiUrl: "https://admin.klyntic.com/backend-api-landing/public/api",
+  apiUrlMedia: "https://admin.klyntic.com/backend-api-landing/storage/app/",
   // clientId: ''
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   //apirest local
-  // apiUrl: "http://127.0.0.1:8000/api",
-  // apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
+  apiUrl: "http://127.0.0.1:8000/api",
+  apiUrlMedia: "http://127.0.0.1:8000/storage/app/",
   //remoto
-  apiUrl: "https://admin.health-connect.me/backend-api-landing/public/api",
-  apiUrlMedia: "https://admin.health-connect.me/backend-api-landing/storage/app/",
+  // apiUrl: "https://admin.klyntic.com/backend-api-landing/public/api",
+  // apiUrlMedia: "https://admin.klyntic.com/backend-api-landing/storage/app/",
   // clientId: ''
 
 };
