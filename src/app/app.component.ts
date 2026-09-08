cc56f1c1e9e293f5b4aa27978d393e958bec9e8d
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-09-08 18:49:53 -0400

    instalado vercel analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { inject } from '@vercel/analytics';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'landinghealthconnectme';
+   ngOnInit() {
+    // Se ejecuta una sola vez al cargar la aplicación
+    inject();
+  }
 }
